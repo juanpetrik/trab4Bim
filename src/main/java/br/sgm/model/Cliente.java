9f15e7e954ce1b92975243fd526c881c0ca1f85c
jpspetrik <jpspetrik@ws-lab01_149.univel.univel.br>
2015-10-27 20:10:50 -0200

    Criação da classe Usuário

--- src/main/java/br/sgm/model/Cliente.java
+++ src/main/java/br/sgm/model/Cliente.java
@@ -14,4 +14,56 @@ public class Cliente {
 	private String email;
 	private Genero genero;
 	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public UF getUf() {
+		return uf;
+	}
+	public void setUf(UF uf) {
+		this.uf = uf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Genero getGenero() {
+		return genero;
+	}
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+	
+	
 }
---
+++ src/main/java/br/sgm/model/Usuario.java
@@ -0,0 +1,28 @@
+package br.sgm.model;
+
+public class Usuario {
+	private int id;
+	private int idCliente;
+	private String senha;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
